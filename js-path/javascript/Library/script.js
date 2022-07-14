083731d99fc5276fb2314981229301ade3deb077
Pesteves2002 <tomasesteves2002@gmail.com>
2022-07-14 23:48:15 +0100

    Finish library Project

--- js-path/javascript/Library/script.js
+++ js-path/javascript/Library/script.js
@@ -17,6 +17,10 @@ function Book(title, author, pages, read) {
       isRead
     );
   };
+
+  this.toggleRead = function () {
+    this.read = !this.read;
+  };
 }
 const theHobbit = new Book("The Hobbit", "someone", 567, false);
 const harryPotter = new Book("Harry Potter", "anyone", 34, true);
@@ -59,15 +63,84 @@ function create_book(book) {
     book_info.classList.add("not-read");
   }
   book_info.classList.add("book");
+  book_info.setAttribute("id", "book" + numBooks);
+
+  let bookButtons = document.createElement("div");
+
+  bookButtons.classList.add("bookButtons");
+  let changeButton = document.createElement("button");
+  changeButton.classList.add("changeButton");
+  changeButton.addEventListener("click", () =>
+    changeReadStatus(book_info.id.split("k")[1])
+  );
+  changeButton.textContent = "Toggle Read";
+
+  bookButtons.appendChild(changeButton);
+
+  let deleteButton = document.createElement("button");
+  deleteButton.classList.add("deleteButton");
+  deleteButton.addEventListener("click", () => deleteBook(book_info.id));
+  deleteButton.textContent = "Delete";
+  bookButtons.appendChild(deleteButton);
+
+  book_info.appendChild(bookButtons);
+
+  numBooks++;
   return book_info;
 }
 
+function changeReadStatus(bookNum) {
+  myLibrary[bookNum].toggleRead();
+  let book = document.getElementById("book" + bookNum);
+
+  // if is read
+  if (book.classList.contains("read")) {
+    book.classList.remove("read");
+    book.classList.add("not-read");
+  }
+  // not read
+  else {
+    book.classList.remove("not-read");
+    book.classList.add("read");
+  }
+}
+function deleteBook(bookId) {
+  let book = document.getElementById(bookId);
+  console.log(book);
+  book.remove();
+}
+
 function add_book_functionality() {
-  const formContainer = document.querySelector("#formContainer");
+  let newBook = document.forms["addBook"];
+  let newBookName = newBook["bookName"].value;
+  let newBookAuthor = newBook["author"].value;
+  let newBookNumPages = newBook["numPages"].value;
+  let newBookRead = newBook["read"].checked;
+
+  if (
+    newBookName == "" ||
+    newBookAuthor == "" ||
+    newBookNumPages == "" ||
+    newBookNumPages <= 0
+  )
+    return;
+
+  const book = new Book(
+    newBookName,
+    newBookAuthor,
+    newBookNumPages,
+    newBookRead
+  );
+  addBookToLibrary(book);
+  let all_books = document.querySelector(".all-books");
+  all_books.appendChild(create_book(book));
 }
 
 function create_webpage() {
   const library = document.querySelector("#library");
+
+  let addButton = document.querySelector(".add-button");
+  addButton.addEventListener("click", () => add_book_functionality());
 
   const container = document.createElement("div");
 
@@ -78,9 +151,9 @@ function create_webpage() {
 
   shelf.appendChild(all_books);
   library.appendChild(shelf);
-
-  add_book_functionality();
 }
+
+let numBooks = 0;
 
 addBookToLibrary(theHobbit);
 addBookToLibrary(theHobbit);
