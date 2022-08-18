4fb88366796b21362d2b07a7fb9fde76e4990ed0
Pesteves2002 <tomasesteves2002@gmail.com>
2022-08-18 14:14:34 +0100

    Fix celsius and bad input given

--- js-path/javascript/weather-app/src/index.js
+++ js-path/javascript/weather-app/src/index.js
@@ -38,42 +38,45 @@ async function UpdateInfo(city) {
 
   console.log(data);
 
-  const coords = data.coord;
-  const main = data.main;
-  const country = data.sys.country;
-  const weather = data.weather[0];
-  const wind = data.wind;
-
   const weatherContainer = document.createElement("div");
   weatherContainer.classList.add("weather-container");
+  if (data.cod != 400) {
+    const coords = data.coord;
+    const main = data.main;
+    const country = data.sys.country;
+    const weather = data.weather[0];
+    const wind = data.wind;
 
-  weatherContainer.innerText =
-    "Status: " + weather.main + " Description:" + weather.description;
+    weatherContainer.innerText =
+      "Status: " + weather.main + " Description:" + weather.description;
 
-  const mainContainer = document.createElement("div");
-  mainContainer.classList.add("main-container");
+    const mainContainer = document.createElement("div");
+    mainContainer.classList.add("main-container");
 
-  mainContainer.innerText = "Feels like: " + main.feels_like;
+    mainContainer.innerText =
+      "Feels like: " + (main.feels_like - 273.15).toFixed(0);
 
-  const windContainer = document.createElement("div");
-  windContainer.classList.add("wind-container");
+    const windContainer = document.createElement("div");
+    windContainer.classList.add("wind-container");
 
-  windContainer.innerText = "Wind: " + wind.speed + " Deg: " + wind.deg;
+    windContainer.innerText = "Wind: " + wind.speed + " Deg: " + wind.deg;
 
-  const countryContainer = document.createElement("div");
-  countryContainer.classList.add("country-container");
+    const countryContainer = document.createElement("div");
+    countryContainer.classList.add("country-container");
 
-  countryContainer.innerText =
-    data.name + ", " + country + "\n " + coords.lat + ", " + coords.lon;
+    countryContainer.innerText =
+      data.name + ", " + country + "\n " + coords.lat + ", " + coords.lon;
 
-  const cloudContainer = document.createElement("div");
-  cloudContainer.classList.add("cloud-container");
+    const cloudContainer = document.createElement("div");
+    cloudContainer.classList.add("cloud-container");
 
-  weatherContainer.appendChild(mainContainer);
-  weatherContainer.appendChild(windContainer);
-  weatherContainer.appendChild(countryContainer);
-  weatherContainer.appendChild(cloudContainer);
-
+    weatherContainer.appendChild(mainContainer);
+    weatherContainer.appendChild(windContainer);
+    weatherContainer.appendChild(countryContainer);
+    weatherContainer.appendChild(cloudContainer);
+  } else {
+    weatherContainer.innerText = "Couldn't locate city";
+  }
   body.appendChild(header);
   body.appendChild(weatherContainer);
 }
