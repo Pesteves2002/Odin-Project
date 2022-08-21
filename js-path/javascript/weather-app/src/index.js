4286d6b98c05fc6ceb2bb915d04a30d262a16b43
Pesteves2002 <tomasesteves2002@gmail.com>
2022-08-21 16:14:23 +0100

    Add linting

--- js-path/javascript/weather-app/src/index.js
+++ js-path/javascript/weather-app/src/index.js
@@ -1,10 +1,7 @@
 async function getData(city) {
   try {
     const response = await fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&appid=" +
-        config.API_KEY
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${config.API_KEY}`
     );
 
     const data = await response.json();
@@ -12,10 +9,11 @@ async function getData(city) {
   } catch (error) {
     console.log(error);
   }
+  return null;
 }
 
 async function UpdateInfo(city) {
-  const body = document.body;
+  const { body } = document;
   body.replaceChildren();
 
   const header = document.createElement("div");
@@ -34,38 +32,37 @@ async function UpdateInfo(city) {
 
   button.addEventListener("click", () => UpdateInfo(citySearch.value));
 
-  let data = await getData(city);
+  const data = await getData(city);
 
   console.log(data);
 
   const weatherContainer = document.createElement("div");
   weatherContainer.classList.add("weather-container");
-  if (data.cod != 400) {
+  if (data.cod !== 400) {
     const coords = data.coord;
-    const main = data.main;
-    const country = data.sys.country;
+    const { main } = data;
+    const { country } = data.sys;
     const weather = data.weather[0];
-    const wind = data.wind;
+    const { wind } = data;
 
-    weatherContainer.innerText =
-      "Status: " + weather.main + " Description:" + weather.description;
+    weatherContainer.innerText = `Status: ${weather.main} Description:${weather.description}`;
 
     const mainContainer = document.createElement("div");
     mainContainer.classList.add("main-container");
 
-    mainContainer.innerText =
-      "Feels like: " + (main.feels_like - 273.15).toFixed(0);
+    mainContainer.innerText = `Feels like: ${(main.feels_like - 273.15).toFixed(
+      0
+    )}`;
 
     const windContainer = document.createElement("div");
     windContainer.classList.add("wind-container");
 
-    windContainer.innerText = "Wind: " + wind.speed + " Deg: " + wind.deg;
+    windContainer.innerText = `Wind: ${wind.speed} Deg: ${wind.deg}`;
 
     const countryContainer = document.createElement("div");
     countryContainer.classList.add("country-container");
 
-    countryContainer.innerText =
-      data.name + ", " + country + "\n " + coords.lat + ", " + coords.lon;
+    countryContainer.innerText = `${data.name}, ${country}\n ${coords.lat}, ${coords.lon}`;
 
     const cloudContainer = document.createElement("div");
     cloudContainer.classList.add("cloud-container");
