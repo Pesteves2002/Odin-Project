6ca517a19a366d8ad66e3f39a8e9401770a82f54
Pesteves2002 <tomasesteves2002@gmail.com>
2022-08-25 14:43:39 +0100

    Add node mini project

---
+++ js-path/node/node-website/index.js
@@ -0,0 +1,40 @@
+const http = require("http");
+const fs = require("fs");
+
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile("index.html", function (err, data) {
+      if (err) throw err;
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end;
+    });
+  } else if (req.url === "/about") {
+    fs.readFile("about.html", function (err, data) {
+      if (err) throw err;
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end;
+    });
+  } else if (req.url === "/contact-me") {
+    fs.readFile("contact-me.html", function (err, data) {
+      if (err) throw err;
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end;
+    });
+  } else {
+    fs.readFile("404.html", function (err, data) {
+      if (err) throw err;
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end;
+    });
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+});
